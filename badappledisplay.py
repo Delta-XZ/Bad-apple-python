66f3721def1bbc1eaa63681b7fb0bd426e698d6b
Delta-XZ <78885232+Delta-XZ@users.noreply.github.com>
2021-02-11 19:43:12 +0200

    Update badappledisplay.py

--- badappledisplay.py
+++ badappledisplay.py
@@ -1,15 +1,26 @@
 import time
 
+#uncomment time.time lines for time monitoring(seconds)
+#Start = time.time()
+
 filenum = 1
-
+#clears the screen
 input()
-print(" ####\n"*59)
+print("\n"*61)
 input()
 
-for i in range(25):
+for i in range(6572):
+    #directory = disk/folder/folder/
+    #change to suit your system
     directory = "G:/BADAPPLE/ASCII/" +  str(filenum)
     file = open(directory + ".txt","r")
     data = file.read()
     print(data)
     filenum += 1
-    time.sleep(0.03333333)
+    time.sleep(0.0149999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+
+#end = time.time()
+
+#Time = end - Start
+
+#print(Time)
