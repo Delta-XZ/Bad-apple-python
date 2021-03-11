c12c94692c38397a0a6d7941c4a5ff612c876d6f
Delta-XZ <78885232+Delta-XZ@users.noreply.github.com>
2021-03-11 15:49:26 +0200

    revised display code

--- badappledisplay.py
+++ badappledisplay.py
@@ -1,13 +1,79 @@
-import time
+import time, fpstimer, os, pathlib
+from playsound import playsound
 
+# get path to files 
+filepath = pathlib.Path(__file__).parent.absolute()
+truefilepath = str(filepath) + str("\\")
+###########
 
-Start = time.time()
+def run():
+    Start = time.time()
+    filenum = 1
+    timer = fpstimer.FPSTimer(float(fps)) 
+    for i in range(files):
+        directory = init_directory
+        directory = directory + str(filenum)
+        file = open(directory + ".txt","r", encoding="utf-8")
+        data = file.read()
+        print(data)
+        filenum += 1
+        timer.sleep() # Pause just enough to have a 1/60 second wait since last fpstSleep() call.
 
-filenum = 1
+    end = time.time()
 
-input()
-print("\n"*100)
-input()
+    Time = end - Start
+    print("""
+    thank you
+    for watching
+    untill the
+    end.
+    love, dev :)
+    """)
+    print(Time)
+    time.sleep(1000)
+
+def menu():
+    global ver
+    print("Choose The Number")
+    ver = input("1. Original 2. Scanline 3. SNES 4. ASCII HQ 5.LOWQ 6. Widescreen: ")
+    print("Are you sure you want to run option ", ver, "?(Y/N)")
+    conf = input()
+
+    while conf == "N":
+        ver = input("1. Original 2. Scanline 3. SNES 4. ASCII 60 FPS 5.LOWQ 6. Widescreen: ")
+        print("Are you sure you want to run option ", ver, "?(Y/N)")
+        conf = input()
+    global fps
+    global files
+    global init_directory
+    if ver == "1":
+        init_directory = "G:/BADAPPLE/ASCII/"
+        fps = int(30)
+        files = int(6571)
+    elif ver == "2":
+        init_directory = "G:/BADAPPLE/SCAN/"
+        fps = int(30)
+        files = int(6571)
+    elif ver == "3":
+        init_directory = str(truefilepath) + "SNES\\"
+        fps = int(10)
+        files = int(2191)
+    elif ver == "4":
+        init_directory = str(truefilepath) + "ASCII_HQ\\"
+        fps = int(60)
+        files = int(13141)
+    elif ver == "5":
+        init_directory = str(truefilepath) + "LOWQ\\"
+        fps = int(60)
+        files = int(13141)
+    elif ver == "6":
+        init_directory = str(truefilepath) + "WIDESCREEN\\"
+        fps = int(60)
+        files = int(13166)
+        
+    input()
+    print("\n"*1000)
+    input()
 
 #LEGACY
 
@@ -30,26 +96,29 @@ input()
 #print(Time)
 
 ###########################################################
+print("Reading files from:", truefilepath)
 
-Start = time.time()
+menu()
+if ver == "1":
+    os.system("mode con cols=94 lines=70")
+    playsounddir = truefilepath + "BADAPPLE.wav"
+    playsound(playsounddir, block = False)
+elif ver == "5":
+    os.system("mode con cols=120 lines=90")
+    playsounddir = truefilepath + "BADAPPLE.wav"
+    playsound(playsounddir, block = False)
+    run()
+elif ver == "3":
+    os.system("mode con cols=32 lines=24")
+    playsounddir = truefilepath + "BADAPPLE_SNES.mp3"
+    playsound(playsounddir, block = False)
+    run()
+elif ver == "6":
+    os.system("mode con cols=208 lines=117")
+    playsounddir = truefilepath + "BADAPPLE.wav"
+    playsound(playsounddir, block = False)
+    run()
 
-import fpstimer
-timer = fpstimer.FPSTimer(30) # Make a timer that is set for 60 fps.
-for i in range(6572): # Each iteration of this loop will last (at least) 1/60 of a second.
-    # Do calculations/work for a single "frame" here.
-    directory = "G:/BADAPPLE/ASCII/" +  str(filenum)
-    file = open(directory + ".txt","r")
-    data = file.read()
-    print(data)
-    filenum += 1
-    timer.sleep() # Pause just enough to have a 1/60 second wait since last fpstSleep() call.
-
-end = time.time()
-
-Time = end - Start
-
-print(Time)
-		
-		
-		
-		
+playsounddir = truefilepath + "BADAPPLE.wav"
+playsound(playsounddir, block = False)
+run()
